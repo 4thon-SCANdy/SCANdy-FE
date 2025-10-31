00b2760c4b95cb0323e2d8d0127dc5c447e0f147
a-neey <2371429@hansung.ac.kr>
2025-10-31 20:38:50 +0900

    ✨ MainPage 좌측 디자인 구현

---
+++ src/pages/mainPage/MainPage.jsx
@@ -0,0 +1,15 @@
+import * as S from "./MainPage.style";
+
+import MainLeftSection from "./components/MainLeftSection";
+
+const MainPage = () => {
+  return (
+    <>
+      <S.MainContainer>
+        <MainLeftSection />
+      </S.MainContainer>
+    </>
+  );
+};
+
+export default MainPage;
---
+++ src/pages/mainPage/MainPage.style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  width: 120rem;
+  height: 67.5rem;
+  flex-shrink: 0;
+  background: #fdfdfd;
+`;
