ecc1115146b8be0decf5b066ba6cf844b0e0b154
a-neey <2371429@hansung.ac.kr>
2025-11-01 15:33:01 +0900

    ✨ 오늘 날짜 컴포넌트 구현

--- src/pages/mainPage/components/MainLeftSection.jsx
+++ src/pages/mainPage/components/MainLeftSection.jsx
@@ -1,16 +1,47 @@
 import * as S from "../styles/MainLeftSection.style";
+import MiniCalendar from "./MiniCalendar";
 
 import LOGO from "@/assets/main/logo.svg";
 import TODAY from "@/assets/main/todayDate.svg";
 import TAG from "@/assets/main/tag.svg";
-import MiniCalendar from "./MiniCalendar";
+import LINE from "@/assets/miniCalendar/line.svg";
+
+const days = [
+  "일요일",
+  "월요일",
+  "화요일",
+  "수요일",
+  "목요일",
+  "금요일",
+  "토요일",
+];
 
 const MainLeftSection = () => {
+  const today = new Date();
+
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const date = today.getDate();
+  const day = days[today.getDay()];
+
   return (
     <>
       <S.LeftSectionContainer>
         <S.Logo src={LOGO} />
-        <S.TodayDate src={TODAY} />
+        <S.DateBox>
+          <S.DateTop>
+            <S.DateTitle>오늘 날짜</S.DateTitle>
+          </S.DateTop>
+          <S.DateBottom>
+            <S.YearText>{year}년</S.YearText>
+            <S.TodayBox>
+              <S.TodayText>{month}월</S.TodayText>
+              <S.TodayText>{date}일</S.TodayText>
+              <S.Line src={LINE} />
+              <S.TodayText>{day}</S.TodayText>
+            </S.TodayBox>
+          </S.DateBottom>
+        </S.DateBox>
         <MiniCalendar />
         <S.TagBox src={TAG} />
       </S.LeftSectionContainer>
--- src/pages/mainPage/styles/MainLeftSection.style.js
+++ src/pages/mainPage/styles/MainLeftSection.style.js
@@ -8,7 +8,7 @@ export const LeftSectionContainer = styled.div`
   flex-direction: column;
   justify-content: flex-end;
   align-items: center;
-  gap: ${vw(28)};
+  gap: ${vw(27)};
   background: linear-gradient(180deg, #fdfdfd 0%, #eaedff 100%);
 `;
 
@@ -17,8 +17,86 @@ export const Logo = styled.img`
   height: ${vw(80)};
 `;
 
-export const TodayDate = styled.img`
+export const DateBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const DateTop = styled.div`
+  display: flex;
+  text-align: center;
+  gap: ${vw(10)};
+  border-radius: ${vw(20)} ${vw(20)} 0 0;
+  background: #fdfdfd;
+  width: ${vw(118)};
+  height: ${vw(40)};
+  box-shadow: 0 0 20px 0 rgba(180, 191, 255, 0.3);
+`;
+
+export const DateTitle = styled.p`
+  color: #4842b2;
+  font-family: Pretendard;
+  padding: ${vw(12)} ${vw(30)};
+  font-size: ${vw(16)};
+  font-style: normal;
+  font-weight: 500;
+  line-height: 100%; /* 16px */
+  letter-spacing: -0.4px;
+`;
+
+export const DateBottom = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: ${vw(355)};
+  height: ${vw(80)};
+  border-radius: 0 ${vw(40)} ${vw(40)} ${vw(20)};
+  background: #fdfdfd;
+  box-shadow: 0 0 20px 0 rgba(180, 191, 255, 0.3);
+  gap: ${vw(13)};
+`;
+
+export const YearText = styled.div`
+  color: #7e8df5;
+  font-family: Pretendard;
+  font-size: ${vw(24)};
+  font-style: normal;
+  font-weight: 500;
+  line-height: 100%; /* 24px */
+  letter-spacing: -0.6px;
+`;
+
+export const TodayBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: ${vw(13)};
+  width: ${vw(229)};
+  height: ${vw(56)};
+  flex-shrink: 0;
+  border-radius: ${vw(30)};
+  background: linear-gradient(
+    90deg,
+    rgba(234, 237, 255, 0.5) 0%,
+    rgba(180, 191, 255, 0.5) 147.61%
+  );
+`;
+
+export const TodayText = styled.p`
+  color: #4842b2;
+  font-family: Pretendard;
+  font-size: ${vw(24)};
+  font-style: normal;
+  font-weight: 500;
+  line-height: 100%; /* 24px */
+  letter-spacing: -0.6px;
+`;
+
+export const Line = styled.img`
+  width: ${vw(2)};
+  height: ${vw(32)};
 `;
 
 export const TagBox = styled.img`
