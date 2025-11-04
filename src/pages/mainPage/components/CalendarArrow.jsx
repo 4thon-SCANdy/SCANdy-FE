f728bef4373c1e6a0a79ab27848af2a878ef98b8
a-neey <2371429@hansung.ac.kr>
2025-11-04 15:28:55 +0900

    ✨ 화살표 호버 및 클릭 기능 추가

--- src/pages/mainPage/components/CalendarArrow.jsx
+++ src/pages/mainPage/components/CalendarArrow.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import * as S from "../styles/CalendarArrow.style";
 
-import PREV from "@/assets/calendar/prev.svg";
-import NEXT from "@/assets/calendar/next-click.svg";
+import PREV_DEFAULT from "@/assets/calendar/prev.svg";
+import PREV_HOVER from "@/assets/calendar/prev-hover.svg";
+import PREV_ACTIVE from "@/assets/calendar/prev-click.svg";
+
+import NEXT_DEFAULT from "@/assets/calendar/next.svg";
+import NEXT_HOVER from "@/assets/calendar/next-hover.svg";
+import NEXT_ACTIVE from "@/assets/calendar/next-click.svg";
 
 const CalendarArrow = ({
   onPrev,
@@ -10,23 +16,33 @@ const CalendarArrow = ({
   height = 46,
   gap = 80,
 }) => {
+  const [prevSrc, setPrevSrc] = useState(PREV_DEFAULT);
+  const [nextSrc, setNextSrc] = useState(NEXT_DEFAULT);
+
   return (
-    <>
-      <S.CalenderArrowContainer $gap={gap}>
-        <S.ArrowImg
-          src={PREV}
-          onClick={onPrev}
-          $width={width}
-          $height={height}
-        />
-        <S.ArrowImg
-          src={NEXT}
-          onClick={onNext}
-          $width={width}
-          $height={height}
-        />
-      </S.CalenderArrowContainer>
-    </>
+    <S.CalenderArrowContainer $gap={gap}>
+      <S.ArrowImg
+        src={prevSrc}
+        $width={width}
+        $height={height}
+        onClick={onPrev}
+        onMouseEnter={() => setPrevSrc(PREV_HOVER)}
+        onMouseLeave={() => setPrevSrc(PREV_DEFAULT)}
+        onMouseDown={() => setPrevSrc(PREV_ACTIVE)}
+        onMouseUp={() => setPrevSrc(PREV_HOVER)}
+      />
+
+      <S.ArrowImg
+        src={nextSrc}
+        $width={width}
+        $height={height}
+        onClick={onNext}
+        onMouseEnter={() => setNextSrc(NEXT_HOVER)}
+        onMouseLeave={() => setNextSrc(NEXT_DEFAULT)}
+        onMouseDown={() => setNextSrc(NEXT_ACTIVE)}
+        onMouseUp={() => setNextSrc(NEXT_HOVER)}
+      />
+    </S.CalenderArrowContainer>
   );
 };
 
