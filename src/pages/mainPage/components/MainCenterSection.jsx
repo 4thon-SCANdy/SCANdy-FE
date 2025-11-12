e3a8b41cc91d4522e189b9de071a53d1daa77913
a-neey <2371429@hansung.ac.kr>
2025-11-13 02:10:46 +0900

    ✨ 일정 검색 API 연동

--- src/pages/mainPage/components/MainCenterSection.jsx
+++ src/pages/mainPage/components/MainCenterSection.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import MainCalendar from "./MainCalendar";
 import CalendarArrow from "./CalendarArrow";
 import SearchBar from "./SearchBar";
+import { calendarSearchApi } from "../../../apis/calendar/calendarSearchApi";
 
 const MainCenterSection = ({
   tags,
@@ -14,7 +15,15 @@ const MainCenterSection = ({
   const [currentDate, setCurrentDate] = useState(new Date());
   const [searchMode, setSearchMode] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
 
+  const getMonthRange = (date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const start = new Date(year, month, 1).toISOString().split("T")[0];
+    const end = new Date(year, month + 1, 0).toISOString().split("T")[0];
+    return { start, end };
+  };
   const handlePrevMonth = () => {
     const newDate = new Date(currentDate);
     newDate.setMonth(currentDate.getMonth() - 1);
@@ -27,6 +36,27 @@ const MainCenterSection = ({
     setCurrentDate(newDate);
   };
 
+  const handleSearch = async (v) => {
+    setSearchQuery(v);
+    if (!v) {
+      setSearchResult([]); // 검색어 없으면 초기화
+      return;
+    }
+
+    const { start, end } = getMonthRange(currentDate);
+    try {
+      const data = await calendarSearchApi(v, start, end);
+      if (data?.data?.length) {
+        setSearchResult(data.data);
+      } else {
+        setSearchResult([]); // 일정 없을 경우
+      }
+    } catch (e) {
+      console.error("검색 실패", e);
+      setSearchResult([]);
+    }
+  };
+
   return (
     <>
       <S.CenterSectionContainer>
@@ -34,7 +64,7 @@ const MainCenterSection = ({
           <SearchBar
             onFocus={() => setSearchMode(true)}
             onBlur={() => setSearchMode(false)}
-            onSearch={(v) => setSearchQuery(v)}
+            onSearch={handleSearch}
           />
           <CalendarArrow
             currentDate={currentDate}
@@ -44,7 +74,7 @@ const MainCenterSection = ({
         </S.CenterHeader>
         <MainCalendar
           tags={tags}
-          schedules={schedules}
+          schedules={searchResult.length ? searchResult : schedules}
           currentDate={currentDate}
           selectedTag={selectedTag}
           onOpenRegister={onOpenRegister}
